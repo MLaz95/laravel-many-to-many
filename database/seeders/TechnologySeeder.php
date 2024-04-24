dc2466f27adc6dc96f52ccdf47f1475e649297c3
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-04-24 15:43:24 +0200

    creato seeder per tabella technology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TechnologySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $technologies = ['html', 'css', 'javascript', 'php', 'vue', 'vite', 'bootstrap', 'laravel'];
+
+        foreach($technologies as $tech){
+            $newTechnology = new Technology();
+            $newTechnology->title = $tech;
+            $newTechnology->color = $faker->safeHexColor();
+            $newTechnology->save();
+        }
     }
 }
